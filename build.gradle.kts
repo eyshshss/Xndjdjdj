5fe928eb65d0c0366579cf974c77bfef9797a42a
baxiy56177 <baxiy56177@amupx.com>
2026-08-02 14:27:15 +0300

    Fix syntax in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  plugins {
     id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.23" apply false
-  alias(libs.plugins.google.devtools.ksp) apply false
-  alias(libs.plugins.roborazzi) apply false
-  alias(libs.plugins.secrets) apply false
-  // alias(libs.plugins.google.services) apply false
 }
